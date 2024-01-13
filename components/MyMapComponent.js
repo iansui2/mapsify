33cb31344b9425a197aa9918c94511ea39b5e5fc
iansui2 <iansui200@gmail.com>
2024-01-13 20:46:10 +0800

    updated location getting and ui updates

--- components/MyMapComponent.js
+++ components/MyMapComponent.js
@@ -2,13 +2,14 @@ import { Box } from "@chakra-ui/react";
 import { useRef, useState, useEffect } from "react";
 import { Fragment, Children, cloneElement, isValidElement } from "react";
 
-export const MyMapComponent = ({ children }) => {
+export const MyMapComponent = ({ children, position }) => {
+  console.log(position)
   const ref = useRef()
   const [map, setMap] = useState()
 
   useEffect(() => {
     if (ref.current && !map) {
-      setMap(new window.google.maps.Map(ref.current, { center: { lat: 14.582919, lng: 120.979683 }, zoom: 16 }));
+      setMap(new window.google.maps.Map(ref.current, { center: { lat: position?.lat, lng: position?.lng }, zoom: 16 }));
     }
   }, [ref, map])
 
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,34 @@
-import { Box, Spinner } from '@chakra-ui/react'
+import { useState, useEffect } from 'react'
+import { Box, Center, Spinner } from '@chakra-ui/react'
 import { Wrapper, Status } from "@googlemaps/react-wrapper"
 import { Marker } from '../components/Marker'
 import { MyMapComponent } from '../components/MyMapComponent'
 import { AppLayout } from '../layouts/AppLayout'
 
 export default function Home() {
+  const [position, setPosition] = useState({ lat: 14.582919, lng: 120.979683 });
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+      });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }, []);
 
   const render = (status) => {
     switch (status) {
       case Status.LOADING:
-        return <Spinner />
+        return (
+          <Center minHeight="100vh">
+            <Spinner size='xl' />
+          </Center>
+        );      
       case Status.SUCCESS:
         return <MyMapComponent />
     }
@@ -19,8 +38,8 @@ export default function Home() {
     <AppLayout>
       <Box>
         <Wrapper apiKey='AIzaSyD6AUPIR0eIiGldIIo0b06uqLxlZDyQh-I' render={render}>
-          <MyMapComponent>
-            <Marker position={{ lat: 14.582919, lng: 120.979683 }} />
+          <MyMapComponent position={position}>
+            <Marker position={position} />
           </MyMapComponent>
         </Wrapper>
       </Box>
